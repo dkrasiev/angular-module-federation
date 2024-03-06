b1b31c8c2317f95770b04b6a428867432821c76c
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-03-06 11:05:10 +0400

    configure routes

--- projects/host/src/app/app.routes.ts
+++ projects/host/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => loadRemoteModule({ type: 'module', remoteEntry: 'http://localhost:4201/remoteEntry.js', exposedModule: './Component' }).then((m) => m.AppComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
--- projects/host/webpack.config.js
+++ projects/host/webpack.config.js
@@ -3,7 +3,7 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "about": "http://localhost:4200/remoteEntry.js",    
+    "about": "http://localhost:4201/remoteEntry.js",
   },
 
   shared: {
